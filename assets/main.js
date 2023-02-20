db18cead01bfaf51b50c88528bfb14f7a5c9b9a6
jbowdle <johnwbowdle@gmail.com>
2023-02-20 13:19:03 -0600

    Added begin button functionality

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,11 @@ const settingsBtn = document.querySelector("#settings-btn");
 const settings = document.querySelector("#settings");
 const closeSettings = document.querySelectorAll(".close-settings");
 const timer = document.querySelector("#timer");
+const skipBack = document.querySelector("#skip-back");
+const startStop = document.querySelector("#start-stop");
+const skipForward = document.querySelector("#skip-forward");
+const controls = document.querySelector("#controls");
+const begin = document.querySelector("#begin");
 
 let minutes = 25;
 let seconds = 00;
@@ -28,36 +33,40 @@ const closeSettingsEvent = function() {
 
 closeSettingsEvent();
 
-// test value
-buttonStatus = true;
-const countdown = function() {
-    if (buttonStatus) {
-        playTimer = setInterval(function() {
-            // Rollover minutes once seconds hit zero
-            if (seconds === 0) {
-                minutes--;
-                seconds = 60;
-            }  
-            // Reduce seconds by one every interval
-            seconds--;
+function countdown() {
+    playTimer = setInterval(function() {
+        // Rollover minutes once seconds hit zero
+        if (seconds === 0) {
+             minutes--;
+            seconds = 60;
+        }  
+        // Reduce seconds by one every interval
+        seconds--;
             
-            // Updates the timer paragraph, ensures that it looks proper
-            if ((seconds < 10) && (minutes > 10)) {
-                timer.textContent =  `${minutes}:0${seconds}`;
-            } else if ((seconds < 10) && (minutes < 10)) {
-                timer.textContent =  `0${minutes}:0${seconds}`;
-            } else if ((seconds >= 10) && (minutes < 10)) {
-                timer.textContent =  `0${minutes}:${seconds}`;
-            } else {
-                timer.textContent =  `${minutes}:${seconds}`;
-            }
+        // Updates the timer paragraph, ensures that it looks proper
+        if ((seconds < 10) && (minutes > 10)) {
+            timer.textContent =  `${minutes}:0${seconds}`;
+        } else if ((seconds < 10) && (minutes < 10)) {
+            timer.textContent =  `0${minutes}:0${seconds}`;
+        } else if ((seconds >= 10) && (minutes < 10)) {
+            timer.textContent =  `0${minutes}:${seconds}`;
+        } else {
+            timer.textContent =  `${minutes}:${seconds}`;
+        }
 
-            // Stops timer at 00:00
-            if (minutes === 0 && seconds === 0) {
-                clearInterval(playTimer);
-            }
-        }, 1000);
-    }
+        // Stops timer at 00:00
+        if (minutes === 0 && seconds === 0) {
+            clearInterval(playTimer);
+        }
+    }, 1000);
 }
 
-countdown();
+const startTimer = function() {
+    countdown();
+    begin.style.display = "none";
+    skipBack.style.display = "inline";
+    startStop.style.display = "inline";
+    skipForward.style.display = "inline";
+}
+
+begin.addEventListener("click", startTimer)
